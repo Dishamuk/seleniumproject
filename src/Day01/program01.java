9337b76a405d7b52a43d6d38d65fb961ce714021
Dishamuk <121844371+Dishamuk@users.noreply.github.com>
2023-02-08 20:06:24 +0530

    update

---
+++ src/Day01/program01.java
@@ -0,0 +1,19 @@
+package Day01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class program01 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+		Select dropdownskill = new Select (driver.findElement(By.xpath("//select[@id='Skills']")));
+		dropdownskill.selectByIndex(2);
+		// TODO Auto-generated method stub
+
+	}
+
+}
